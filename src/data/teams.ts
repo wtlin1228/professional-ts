77848d36a085ce9755428cf835491839119754ec
wtlin <wtlin1228@gmail.com>
2021-02-16 17:08:55 +0800

    feat(migrate-to-ts): types at runtime

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -1,10 +1,34 @@
 import type { ITeam } from '../types';
 import { apiCall } from '../utils/networking';
 
+function isITeam(arg: any): arg is ITeam {
+  return (
+    typeof arg.iconUrl === 'string' &&
+    typeof arg.name === 'string' &&
+    typeof arg.id === 'string' &&
+    Array.isArray(arg.channels)
+  );
+}
+
+function assertIsTypedArray<T>(
+  arg: any,
+  check: (val: any) => val is T,
+): asserts arg is T[] {
+  if (!Array.isArray(arg)) {
+    throw new Error(`Not an array: ${JSON.stringify(arg)}`);
+  }
+  if (arg.some((item) => !check(item))) {
+    throw new Error(`Violators found: ${JSON.stringify(arg)}`);
+  }
+}
+
 let cachedAllTeamsList: Promise<ITeam[]>;
 export async function getAllTeams(): Promise<ITeam[]> {
   if (typeof cachedAllTeamsList === 'undefined')
-    cachedAllTeamsList = apiCall('teams');
+    cachedAllTeamsList = apiCall('teams').then((rawData) => {
+      assertIsTypedArray(rawData, isITeam);
+      return rawData;
+    });
 
   return await cachedAllTeamsList;
 }
